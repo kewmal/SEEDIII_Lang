be3af68396f7e8d6849401028a13b801ea0fb471
Kewin Malicki <108068941+kewmal@users.noreply.github.com>
2023-10-06 14:03:43 +0200

    Add files via upload

---
+++ SeedIII_FREE_CODEINSTALL.bat
@@ -0,0 +1,9 @@
+@echo off
+dd if=C:/[YOU WRITE]/myoscode.txt of=C:/[YOU WRITE]/SeedIII_FREE.img seek=5
+pause
+echo Twoj kod zostal wgrany do dysku .img
+echo Teraz mozesz go wlaczyc na np. QEMU, DosBox, BOCHS lub zainstalowac na USB 
+echo Made by Kewin Malicki
+echo Nowa droga w tworzeniu systemu operacyjnego! 
+pause
+exit
